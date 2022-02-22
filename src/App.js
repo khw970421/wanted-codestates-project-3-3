b6f2c7da217583b2da055e4becef8d81283c2576
khw970421 <khw970421@kakao.com>
2022-02-22 21:01:25 +0900

    ♻️ refactor : 주석 추가 및 코드 개선

--- src/App.js
+++ src/App.js
@@ -2,32 +2,35 @@ import DualSelector from "./components/DualSelector";
 import { useState } from "react";
 import emojiMenus from "./components/data";
 const App = () => {
+  // titleName
   const [availableName, setAvailableName] = useState("available options");
   const [selectedName, setSelectedName] = useState("selected options");
+  // available에서 검색 및 기본으로 사용하는 렌더링 값
   const [availableOptionsArr, setAvailableOptionsArr] = useState(
     emojiMenus.filter((val) => !val.visible)
   );
+  // available에서 검색으로 삭제된(filter) 기존의 값을 저장하는 값
   const [availableSaveOptionsArr, setAvailableSaveOptionsArr] = useState(
     emojiMenus.filter((val) => !val.visible)
   );
+  // selected에서 검색 및 기본으로 사용하는 렌더링 값
   const [selectedOptionsArr, setSelectedOptionsArr] = useState(
     emojiMenus.filter((val) => val.visible)
   );
+  // selected에서 검색으로 삭제된(filter) 기존의 값을 저장하는 값
   const [selectedSaveOptionsArr, setSelectedSaveOptionsArr] = useState(
     emojiMenus.filter((val) => val.visible)
   );
-  const onChangeAvailable = (e) => {
-    setAvailableName(e.target.value);
-  };
 
-  const onChangeSelected = (e) => {
-    setSelectedName(e.target.value);
-  };
+  // input에 따라 title 값 변경
+  const onChangeAvailable = ({ target }) => setAvailableName(target.value);
+  const onChangeSelected = ({ target }) => setSelectedName(target.value);
+
+  // 전체 items 옮기는 핸들러
   const moveAllSelected = () => {
     const res = availableSaveOptionsArr.map((val) => {
       return { ...val, visible: true };
     });
-    console.log(selectedOptionsArr, selectedSaveOptionsArr, res);
     setSelectedOptionsArr([...selectedSaveOptionsArr, ...res]);
     setSelectedSaveOptionsArr([...selectedSaveOptionsArr, ...res]);
     setAvailableSaveOptionsArr([]);
@@ -37,7 +40,6 @@ const App = () => {
     const res = selectedSaveOptionsArr.map((val) => {
       return { ...val, visible: true };
     });
-    console.log(availableOptionsArr, availableSaveOptionsArr, res);
     setAvailableOptionsArr([...availableSaveOptionsArr, ...res]);
     setAvailableSaveOptionsArr([...availableSaveOptionsArr, ...res]);
     setSelectedOptionsArr([]);
@@ -53,11 +55,12 @@ const App = () => {
           setAvailableOptionsArr(res);
         }}
       />
+      {/* 버튼 태그에서 전체 옮기기 버튼 예시 */}
       <button onClick={moveAllSelected}>selected로 모두 옮기기</button>
       <button onClick={moveAllAvailable}>available로 모두 옮기기</button>
+      {/* 환경설정에서 사용하는 input 태그 이벤트 예시 */}
       <input onChange={onChangeAvailable} value={availableName} />
       <input onChange={onChangeSelected} value={selectedName} />
-
       <DualSelector
         titleName={selectedName}
         optionsArr={selectedOptionsArr}
--- src/components/DualSelector.jsx
+++ src/components/DualSelector.jsx
@@ -1,29 +1,38 @@
-
 import { useState } from "react";
 
-const DualSelector = ({ titleName,optionsArr,saveOptionsArr,changeOptionsArr }) => {
-  
-  const [checkedItemCount,setCheckedItemCount] = useState(0)
+const DualSelector = ({
+  titleName,
+  optionsArr,
+  saveOptionsArr,
+  changeOptionsArr,
+}) => {
+  // Todo : 선택시 Count값 증가시키기
+  const [checkedItemCount, setCheckedItemCount] = useState(0);
 
-  const checkValue = ({ target }) => {
+  // 검색한 단어가 존재하는지 체크하고 이에따라 changeOptionsArr 함수 실행
+  const searchValue = ({ target }) => {
     const res = saveOptionsArr.filter(({ name }) => {
       return name.includes(target.value);
     });
     changeOptionsArr(res);
   };
-  const selectTarget = (e)=>{
-    console.log(e.target)
-  }
+
+  // Todo : 선택된 태그를 감지하는 핸들러로 선택된 것에 대한 체크 처리 필요
+  const selectTarget = (e) => {
+    console.log(e.target);
+  };
   return (
     <div>
-      <input type="text" onChange={checkValue}></input>
+      <input type="text" onChange={searchValue}></input>
       <header>{titleName}</header>
       <ul>
         {optionsArr.map((val, idx) => (
           <li key={idx} onClick={selectTarget}>{`${val.emoji} ${val.name}`}</li>
         ))}
       </ul>
-      <div>{checkedItemCount}/{optionsArr.length}</div>
+      <div>
+        {checkedItemCount}/{optionsArr.length}
+      </div>
     </div>
   );
 };
