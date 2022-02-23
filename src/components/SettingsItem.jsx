b66a01b99322d17f0925f06a23bcd934457a90e3
sana0803 <sana0803@naver.com>
2022-02-23 17:19:23 +0900

    ✨ feat : toggle button

--- src/components/SettingsItem.jsx
+++ src/components/SettingsItem.jsx
@@ -4,19 +4,21 @@ const SettingsItem = ({ title, setItems }) => {
   const check = useRef();
   const clickCheckbox = () => {
     setItems(check.current.checked);
+    console.log(setItems)
   };
 
   return (
     <div className="setting-item">      
       <p>{title}</p>
-      <div>
+      <div className={"switch-box"}>
         <input
           onClick={clickCheckbox}
           ref={check}
           type="checkbox"
           id="switch"
+          className="switch-checkbox"
         />
-        <label htmlFor="switch">
+        <label htmlFor="switch" className="switch-label">
           <div className="ball"></div>
         </label>
       </div>
