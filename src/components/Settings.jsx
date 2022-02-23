a47275bb9e95f09cf80e84d48b6de557da9316b0
NamgyungKim <namgyung.kim@gmail.com>
2022-02-23 21:58:58 +0900

    🚚 move: file이름 변경

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import ChaingeSize from "./ChaingeSize";
+import ChangeSize from "./ChangeSize";
 import InputText from "./InputText";
 import SettingsItem from "./SettingsItem";
-import InputRadio from './InputRadio'
-import '../scss/Settings.scss';
+import InputRadio from "./InputRadio";
+import "../scss/Settings.scss";
 
 import { HiCog } from "react-icons/hi";
 
@@ -18,43 +18,59 @@ const Settings = (props) => {
     setItemSizeRadio,
     screenSizeInput,
     setScreenSizeInput,
-  } = props
+  } = props;
 
   const [titleChecked, settitleCheck] = useState(false);
   const [showSetting, setShowSetting] = useState(false);
 
-
   return (
     <div className="settings-wrap">
-      <button onClick={() => { setShowSetting(!showSetting) }} className="settings-btn">
+      <button
+        onClick={() => {
+          setShowSetting(!showSetting);
+        }}
+        className="settings-btn"
+      >
         <HiCog color="#333" size="20" />
       </button>
       <div className="settings">
-        {
-          showSetting
-          ? <div>
-              <SettingsItem id={'title'} title={'타이틀'} setItems={settitleCheck} />
-              {
-                titleChecked
-                  ? <InputText textInput={titleInput} setTextInput={setTitleInput} />
-                  : null
-              }
-              <SettingsItem id={'search'} title={'검색'} setItems={setSearchChecked} />
-              <SettingsItem id={'lift'} title={'하나씩만 옮기기'} setItems={setUnitMoveChecked} />
-              <SettingsItem id={'count'} title={'선택된 아이템 갯수 표시'} setItems={setSelectedItemsChecked} />
-              <InputRadio
-                title={"아이템 크기"}
-                itemSizeRadio={itemSizeRadio}
-                setItemSizeRadio={setItemSizeRadio}
-                className="item-size-box"
-              />
-              <ChaingeSize
-                textInput={screenSizeInput}
-                setTextInput={setScreenSizeInput}
-              />
-            </div>
-            : null
-        }
+        {showSetting ? (
+          <div>
+            <SettingsItem
+              id={"title"}
+              title={"타이틀"}
+              setItems={settitleCheck}
+            />
+            {titleChecked ? (
+              <InputText textInput={titleInput} setTextInput={setTitleInput} />
+            ) : null}
+            <SettingsItem
+              id={"search"}
+              title={"검색"}
+              setItems={setSearchChecked}
+            />
+            <SettingsItem
+              id={"lift"}
+              title={"하나씩만 옮기기"}
+              setItems={setUnitMoveChecked}
+            />
+            <SettingsItem
+              id={"count"}
+              title={"선택된 아이템 갯수 표시"}
+              setItems={setSelectedItemsChecked}
+            />
+            <InputRadio
+              title={"아이템 크기"}
+              itemSizeRadio={itemSizeRadio}
+              setItemSizeRadio={setItemSizeRadio}
+              className="item-size-box"
+            />
+            <ChangeSize
+              textInput={screenSizeInput}
+              setTextInput={setScreenSizeInput}
+            />
+          </div>
+        ) : null}
       </div>
     </div>
   );
