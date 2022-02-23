a946b0cb3de1cec8e54ee609b6d2cf6febd3de00
khw970421 <khw970421@kakao.com>
2022-02-23 13:29:53 +0900

    🚑 hotfix : dualSelector DragAndDrop 문제 해결

--- src/components/DualSelector.jsx
+++ src/components/DualSelector.jsx
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import emojiMenus from "./data";
 
-const DualSelector = ({ titleName,
+const DualSelector = ({
+  titleName,
   optionsArr,
   saveOptionsArr,
-  changeOptionsArr, }) => {
+  changeOptionsArr,
+}) => {
   // options는 props
   const [selectedOptions, setSelectedOptions] = useState([]);
   const [checkedItemCount, setCheckedItemCount] = useState(0);
@@ -95,14 +97,54 @@ const DualSelector = ({ titleName,
     }
   };
 
+  //윤성님 코드
+  const [list, setList] = useState(optionsArr);
+  //useRef를 통해 drag되는 아이템의 인덱스와 dragOver되는 아이템의 인덱스를 current에 저장한다.
+  //드래깅 되는 아이템의 인덱스를 useRef객체의 current에 저장한다.
+  const dragItem = useRef();
+  // 드래깅 되어 지나가는 아이템들의 인덱스를 useRef 객체의 current에 저장한다.
+  const dragOverItem = useRef();
+
+  const onDragStart = (e, position) => {
+    dragItem.current = position;
+    e.target.classList.add("grabbing");
+  };
+  const onDragEnter = (e, position) => {
+    dragOverItem.current = position;
+    const copyListItems = [...list];
+    const dragItemContent = copyListItems[dragItem.current];
+    // 얕은 복사로 만든 카피 배열에서 드래깅되는 아이템을 하나 제거해주고
+    copyListItems.splice(dragItem.current, 1);
+    // 카피 리스트 배열에서 드레깅되는 아이템이 지나간 아이템의 인덱스에 드레그된 아이템을 추가해준다.
+    copyListItems.splice(dragOverItem.current, 0, dragItemContent);
+    // 드래깅된 아이템의 장소를 드래그 오버된 아이템의 인덱스로 바꾸어준다.
+    dragItem.current = dragOverItem.current;
+    // 드래그 오버 아이템의 useRef객체의 current 값을 초기화해준다.
+    dragOverItem.current = null;
+    // 리스트를 새롭게 랜더링할 수 있도록 상태를 업데이트해준다.
+    setList(copyListItems);
+  };
+
+  const onDragEnd = (e) => {
+    e.target.classList.remove("grabbing");
+  };
+
   return (
     <div>
       <ul>
-        {optionsArr?.map((option, idx) => {
+        {list?.map((option, idx) => {
           const { id, emoji, nameKo } = option;
 
           return (
-            <li key={id}>
+            <li
+              key={id}
+              onClick={selectTarget}
+              onDragStart={(e) => onDragStart(e, idx)}
+              onDragEnter={(e) => onDragEnter(e, idx)}
+              onDragOver={(e) => e.preventDefault()}
+              onDragEnd={onDragEnd}
+              draggable
+            >
               <button
                 className={
                   selectedOptions.includes(idx)
@@ -121,5 +163,4 @@ const DualSelector = ({ titleName,
   );
 };
 
-
-export default DualSelector
+export default DualSelector;
